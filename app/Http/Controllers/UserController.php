849df1370fed21da3ddb9c9884fac1ee34ceaa48
Fadi Hatem <fadyhatem@gmail.com>
2020-10-10 00:42:07 +0300

    Create User and Task Controllers

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,41 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+
+use http\Env\Request;
+
+class UserController extends Controller
+{
+    /**
+     * Create a new controller instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    public function index()
+    {
+        //
+    }
+    public function store()
+    {
+        //
+    }
+    public function show()
+    {
+        //
+    }
+    public function update()
+    {
+        //
+    }
+    public function destroy()
+    {
+        //
+    }
+}
