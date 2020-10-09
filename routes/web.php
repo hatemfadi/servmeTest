23f2b8812bf39eed42b2195d69adab22158b6f35
Fadi Hatem <fadyhatem@gmail.com>
2020-10-10 00:41:34 +0300

    Create route file

--- routes/web.php
+++ routes/web.php
@@ -14,3 +14,15 @@
 $router->get('/', function () use ($router) {
     return $router->app->version();
 });
+//Users
+$app->get('/users/', 'UserController@index');
+$app->post('/users/', 'UserController@store');
+$app->get('/users/{user_id}', 'UserController@show');
+$app->put('/users/{user_id}', 'UserController@update');
+$app->delete('/users/{user_id}', 'UserController@destroy');
+//Tasks
+$app->get('/tasks','PostController@index');
+$app->post('/tasks','PostController@store');
+$app->get('/tasks/{post_id}','PostController@show');
+$app->put('/tasks/{post_id}', 'PostController@update');
+$app->delete('/tasks/{post_id}', 'PostController@destroy');
